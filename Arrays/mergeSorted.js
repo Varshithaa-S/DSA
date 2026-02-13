292827ebe96eeb5ffce7e516e797c9ad07bc8c66
Varshitha <varshitha.s@aprecomm.ai>
2026-02-13 12:22:14 +0530

    merge

---
+++ Arrays/mergeSorted.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+// var merge = function(nums1, m, nums2, n) {
+// let nums1copy = nums1.slice(0,m);
+// let p1=0
+// let p2=0;
+// for(let i=0;i<(m+n);i++) {
+//     if((p2>=n)||(nums1copy[p1] < nums2[p2] && p1<m)){
+//         nums1[i] = nums1copy[p1]
+//         p1++;
+//     } else {
+//         nums1[i] = nums2[p2];
+//         p2++
+//     }
+// }
+// };
+
+var merge = function(nums1, m, nums2, n) {
+let nums1copy = nums1.slice(0,m);
+let p1=m-1
+let p2=n-1;
+for (let i=(m+n-1);i>=0;i--) {
+    if(p2<0) {
+        break;
+    }
+
+    if(p1>=0  && nums1[p1] > nums2[p2]) {
+        nums1[i] = nums1[p1]
+        p1--;
+    } else {
+        nums1[i] = nums2[p2];
+        p2--;
+    }
+}
+};
