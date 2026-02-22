73fdcb250b0a58efc5a8df77acf73d5c27a363f3
Varshitha <varshitha.s@aprecomm.ai>
2026-02-22 21:45:42 +0530

    sumOfNOdd

--- Recursion/sumOfN.js
+++ Recursion/sumOfN.js
@@ -1,6 +1,21 @@
-let arr =[5,3,7,2,8]
-function sumOfN(n) {
-    if (n == 0) return arr[0];
-    return arr[n] + sumOfN(n-1)
+let arr =[5,3,2,10,1]
+// function sumOfN(n) {
+//     if (n == 0) return arr[0];
+//     return arr[n] + sumOfN(n-1)
+// }
+// console.log(sumOfN(arr.length-1))
+
+console.log(sumOfNOdd(arr.length-1))
+
+function sumOfNOdd(n) {
+    if (n == 0) 
+        if (arr[n]%2 !== 0) return arr[0];
+        else return 0;
+    
+
+    if (arr[n]%2 !== 0) {
+        return arr[n] + sumOfNOdd(n-1)
+    } else {
+        return sumOfNOdd(n-1)
+    }
 }
-console.log(sumOfN(arr.length-1))
