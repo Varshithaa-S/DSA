a6e5f10f523c1ea91c4c11a06c09c40bb8845a00
Varshitha <varshitha.s@aprecomm.ai>
2026-02-24 16:50:40 +0530

    binary

---
+++ searchingAndSorting/binary.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l = 0;
+    let r = nums.length-1;
+    // let isGot = false
+    
+        let middle;
+    while (r>=l) {
+        middle = Math.floor((l+r)/2);
+        if (nums[middle] == target) {
+            // isGot = true
+            return middle;
+        } else if (nums[middle]<target) {
+            l = middle +1;
+        } else if (nums[middle]>target) {
+            r = middle -1;
+        }
+
+    }
+    return -1
+};
