07087bab3a6605b69395d23f7f7eeb289bd6f1ca
Varshitha <varshitha.s@aprecomm.ai>
2026-03-22 10:15:24 +0530

    reverseList

--- linkedList/middleNode.js
+++ linkedList/middleNode.js
@@ -23,9 +23,8 @@ var middleNode = function(head) {
     // } 
     // head = curr
     // return head;
-    let s = head;
-    let f = head;
-    while(f!=null && f.next!=null) {
+    let s = f = head;
+    while(f && f.next) {
         s = s.next;
         f = f.next.next;
     }
