8a04c240918b4475373fcb87c2112f8b74be6029
Varshitha <varshitha.s@aprecomm.ai>
2026-03-22 09:38:34 +0530

    middleNode

---
+++ linkedList/middleNode.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    // let curr1 = head;
+    // let size =0 ;
+    // while(curr1 !== null) {
+    //     size++;
+    //     curr1 = curr1.next;
+
+    // }
+    // let index = Math.floor(size / 2);
+    // for (let i =0; i< index; i++) {
+    //     curr = curr.next;
+    // } 
+    // head = curr
+    // return head;
+    let s = head;
+    let f = head;
+    while(f!=null && f.next!=null) {
+        s = s.next;
+        f = f.next.next;
+    }
+    return s;
+    
+};
