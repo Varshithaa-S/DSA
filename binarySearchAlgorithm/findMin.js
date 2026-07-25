314cea7b6f50a0ce3d7f7c605b2d4087e4a1e923
Varshitha <varshitha.s@aprecomm.ai>
2026-07-25 18:13:03 +0530

    findMin

---
+++ binarySearchAlgorithm/findMin.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(arr) {
+    let l=0;
+    let r=arr.length-1;
+    
+    while(l<=r) {
+        if(arr[l] <= arr[r]) return arr[l]
+        let m = l+ Math.floor((r-l)/2);
+        
+        if(arr[m] < arr[m-1]) return arr[m];
+        if(arr[l]<arr[r]) return arr[l]
+        else if(arr[m]<arr[l]) r = m-1;
+        else l=m+1;
+
+    }
+
+    // return l;
+};
