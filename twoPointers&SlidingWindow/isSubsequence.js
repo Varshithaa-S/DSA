5b1e5789baea1f5dbc2d1e54745e753efd7fc62b
Varshitha <varshitha.s@aprecomm.ai>
2026-07-28 11:43:56 +0530

    isSubsequence

---
+++ twoPointers&SlidingWindow/isSubsequence.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let i = 0;
+    let j = 0 
+    while(j<t.length) {
+        if(s[i] == t[j]) i++;
+        j++;
+    }
+    return s.length==i
+};
