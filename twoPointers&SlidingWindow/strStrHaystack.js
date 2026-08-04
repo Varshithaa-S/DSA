d0d4c9ab01fe4e205a459b633ff0814faf5edcd0
Varshitha <varshitha.s@aprecomm.ai>
2026-08-04 18:09:57 +0530

    strstrHaystack

---
+++ twoPointers&SlidingWindow/strStrHaystack.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    
+    let n = haystack.length;
+    let m= needle.length;
+
+    for(let i=0;i<=(n-m); i++) {
+        let j;
+        for(j=0;j<m;j++) {
+            if(haystack[i+j] !== needle[j]) break;
+        }
+        if(j==m) return i
+    }
+    return -1;
+
+};
