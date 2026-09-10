569b780aea8a97351c5be08ba18f6f33a902c00e
Varshitha <varshitha.s@aprecomm.ai>
2026-09-10 16:45:56 +0530

    kmp algorithm

---
+++ twoPointers&SlidingWindow/strStr.js
@@ -0,0 +1,61 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    //sliding window
+    // let n   = haystack.length;
+    // let m= needle.length;
+
+    // for(let i=0;i<=(n-m); i++) {
+    //     let j;
+    //     for(j=0;j<m;j++) {
+    //         if(haystack[i+j] !== needle[j]) break;
+    //     }
+    //     if(j==m) return i
+    // }
+    // return -1;
+
+    // =========================================
+    // kmp algorithm
+    let n = haystack.length;
+    let m = needle.length;
+    let lps = [0]
+    let i=0;
+    let j=1;
+
+    while(j<m) {
+        if(needle[i] == needle[j]) {
+            lps[j] = i+1;
+            i++;j++
+        } else {
+            if(i==0) {
+                lps[j] = 0;
+                j++;
+            } else {
+                i = lps[i-1]
+            }
+        }
+    }
+
+    i=j=0;
+    while(i<n) {
+        if(haystack[i] == needle[j]) {
+            i++;
+            j++;
+        } else {
+            if(j==0) {
+                i++;
+            }else {
+                j=lps[j-1]
+            }
+        }
+
+        if(j==m) return i-m;
+    }
+    return -1;
+
+
+
+};
